2cbbc6f1b37ed9261c63b5268ea8f0354e225380
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2023-02-22 21:15:20 +0100

    add loading

--- www/index.js
+++ www/index.js
@@ -3,5 +3,15 @@ import {gsap} from './gsap-public/esm/index.js';
 import {ScrollTrigger} from './gsap-public/esm/ScrollTrigger.js';
 gsap.registerPlugin(ScrollTrigger)
 
+// const loader = document.getElementById('loader')
+// window.addEventListener('load', () =>{
 
+//     if(sessionStorage.getItem('premierChargement') === null){
+//         loader.classList.add('loading-out');
+//     } else {
+//         loader.style.display = "none"; 
+//     }
 
+        
+//     sessionStorage.setItem('premierChargement', 'done');
+// })
